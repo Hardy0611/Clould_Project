b728cce7d548e281cb8e854a4ca3dac0d383c740
Hardy <hardy110602@gmail.com>
2024-11-30 14:48:36 +0800

    done done done

--- controllers/allocation_controller.py
+++ controllers/allocation_controller.py
@@ -6,44 +6,34 @@ from PIL import Image
 import io
 
 def split_model(model, select_layer):
-    split_layer = model.get_layer(select_layer)
-    model1 = Model(inputs=model.layers[0].input, outputs=split_layer.output)
+    split_layer = model.get_layer(select_layer).input
+    model1 = Model(inputs=model.layers[0].input, outputs=split_layer)
 
-    model2_input = split_layer.output
-    model2_layers = model.layers[model.layers.index(split_layer) + 1:]
-    x = model2_input
-    for layer in model2_layers:
-        x = layer(x)
-
-    model2 = Model(inputs=model2_input, outputs=x)
-
-    return model1, model2
+    return model1
 
 def dynamic_allocate():
-    model = load_model("/Users/tszhofan/Documents/Coding/Clould_Project/data/model/cnn_emotion_detection.h5")
-    layers = ["dropout", "dropout_1", "dropout_2"]
+    model = load_model("/Users/tszhofan/Documents/Coding/Clould_Project/data/model/face_model.h5")
+    layers = ["conv2d", "conv2d_1", "conv2d_2", "conv2d_3"]
     cpu_usage = psutil.cpu_percent()
-    spilt = 99
+    stage = 99
     if cpu_usage == 0:
+        select_layer = layers[3]
+        stage = 3
+    elif cpu_usage < 70 and cpu_usage > 0: 
         select_layer = layers[2]
-        spilt = 2
-    elif cpu_usage < 10 and cpu_usage > 0: 
+        stage = 2
+    elif cpu_usage >= 70 and cpu_usage < 95: 
         select_layer = layers[1]
-        spilt = 1
-    else: 
+        stage = 1
+    else:
         select_layer = layers[0]
-        spilt = 0
+        stage = 0
 
-    model1, model2 = split_model(model, select_layer)
-    return model1, model2, spilt
+    model1 = split_model(model, select_layer)
+    return model1, stage
 
 def run_model(model, img_bytes): 
     img = Image.open(io.BytesIO(img_bytes)).convert("L").resize((48, 48))
     img_array = np.array(img).reshape(1, 48, 48, 1).astype('float32')
     result = model.predict(img_array)
     return result
-
-def get_label(emotions_pred): 
-    emotions = ['fearful','disgust','surprise','happy', 'neutral','sad', 'angry']
-    idx = np.argmax(emotions_pred)
-    return emotions[idx]
--- view/app.py
+++ view/app.py
@@ -4,9 +4,9 @@ import os
 import requests, json
 import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from controllers.allocation_controller import dynamic_allocate, run_model, get_label
+from controllers.allocation_controller import dynamic_allocate, run_model
 
-url = "https://fsiz1djvx9.execute-api.ap-southeast-1.amazonaws.com/Prod/send"
+url = "https://nxrqlgpnzzzh3vgkxhvy37asde0atgvb.lambda-url.us-east-1.on.aws/"
 
 st.title("Image Upload")
 
@@ -16,16 +16,14 @@ if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.read()
     st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
-    model1, model2, spilt = dynamic_allocate()
+    model1, stage = dynamic_allocate()
     result = run_model(model1, bytes_data)
-    data = {"result": result.tolist(), "case": spilt}
+    data = {"input": str(result.tolist()), "stage": stage}
 
     # send to AWS lambda function
-    response = requests.post(url, data=json.dumps(data))
-    print(response.text)
-    print(response.status_code)
-    response_data = json.loads(response.text)
-    response_data = np.array(response_data.get("message"))
-
-    emotion = get_label(response_data)
+    header = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=data, headers=header)
+    # print(response.text)
+    emotion = response.json()['emotion']
+    print(emotion)
     st.title(emotion)
